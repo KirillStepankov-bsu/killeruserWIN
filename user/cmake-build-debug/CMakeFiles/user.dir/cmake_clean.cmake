7ef3dd500158d2db2eda34c9fb510ccbc3f265ec
Kirill Stepankov <fpm.stepanko@bsu.by>
2022-12-18 21:41:30 +0300

    itnit

---
+++ user/cmake-build-debug/CMakeFiles/user.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/user.dir/main.cpp.obj"
+  "libuser.dll.a"
+  "user.exe"
+  "user.exe.manifest"
+  "user.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/user.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
